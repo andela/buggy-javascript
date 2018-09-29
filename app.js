c9a3dd3391153bde520fa2541a2c1bc4e0478f49
Olufisayo Bamidele <fisiwizy@gmail.com>
2018-09-29 10:10:23 +0100

    fix inconsistencies and general impromements
    use arrow functions for callbacks. move all function delcalrations to the top scope
    remove unused code
    replace string concatenation with template string

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const  usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -15,19 +15,21 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false,
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter)
+app.use('/users', usersRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.send('Hoorah! We got to the user resource');
 });
 
